61f9aee5fde5a28b28b9bd56f5c198991d1899a4
unknown <sundrie42@gmail.com>
2018-12-05 19:24:27 +0100

    Création de notre fenêtre

--- src/com/labyrinth/yugioh/Main.java
+++ src/com/labyrinth/yugioh/Main.java
@@ -3,7 +3,8 @@ package com.labyrinth.yugioh;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+
+        Window gameWindow = new Window();
+        gameWindow.createWindow();
     }
 }
---
+++ src/com/labyrinth/yugioh/Window.java
@@ -0,0 +1,12 @@
+package com.labyrinth.yugioh;
+// On importe la librairie pour faire des fenêtres
+import javax.swing.JFrame;
+
+
+public class Window {
+    // La méthode pour créer notre fenêtre
+    public static void createWindow(){
+        JFrame myWindow = new JFrame();
+        myWindow.setVisible(true);
+    }
+}

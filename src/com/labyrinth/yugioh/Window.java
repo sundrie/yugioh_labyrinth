e9e06c25c2e41233e8e24ea7f9141e2cfcab3f87
unknown <sundrie42@gmail.com>
2018-12-05 19:36:54 +0100

    Définition des bases constituant notre fenêtre (taille, titre,etc.)

--- src/com/labyrinth/yugioh/Window.java
+++ src/com/labyrinth/yugioh/Window.java
@@ -7,6 +7,14 @@ public class Window {
     // La méthode pour créer notre fenêtre
     public static void createWindow(){
         JFrame myWindow = new JFrame();
+
+        myWindow.setTitle("Yu-Gi-Oh! Labyrinth");
+        myWindow.setSize(1280, 720);
+        // Centre la fenêtre
+        myWindow.setLocationRelativeTo(null);
+        // Termine le processus lorsqu'on clique sur la croix rouge
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         myWindow.setVisible(true);
     }
 }

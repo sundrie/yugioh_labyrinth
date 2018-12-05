62cda4da471d73f712d47585e7e761338c36a3a3
unknown <sundrie42@gmail.com>
2018-12-05 21:04:51 +0100

    Ajout du content panel où un texte est affiché notre class Window aeu des changements (remplacement de myWindow par this notamment)

---
+++ src/com/labyrinth/yugioh/ContentPanel.java
@@ -0,0 +1,14 @@
+package com.labyrinth.yugioh;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class ContentPanel extends JPanel {
+    // Cette méthode est fournie de base par jpanel grâce à extends JPanel on peut y accéder
+    public void paintComponent(Graphics g){
+        // getWidth() et getHeight() retournent la width et height de la fenêtre en divisant par 2 on centre notre contenu
+        g.drawString("Hello world !",this.getWidth()/2,this.getHeight()/2);
+    }
+
+
+}
--- src/com/labyrinth/yugioh/Window.java
+++ src/com/labyrinth/yugioh/Window.java
@@ -2,22 +2,24 @@ package com.labyrinth.yugioh;
 // On importe la librairie pour faire des fenêtres
 import javax.swing.JFrame;
 
-
 public class Window extends JFrame {
     int width = 1280;
     int height = 720;
     String title = "Yu-Gi-Oh! Labyrinth";
     // La méthode pour créer notre fenêtre
     public void createWindow(){
-        JFrame myWindow = new JFrame();
-
-        myWindow.setTitle(title);
-        myWindow.setSize(width, height);
+        this.setTitle(title);
+        this.setSize(width, height);
         // Centre la fenêtre
-        myWindow.setLocationRelativeTo(null);
+        this.setLocationRelativeTo(null);
         // Termine le processus lorsqu'on clique sur la croix rouge
-        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        myWindow.setVisible(true);
+        // on dit à Window que notre classe ContentPanel est son contentPane
+        this.setContentPane(new ContentPanel());
+
+        this.setVisible(true);
     }
+
+
 }

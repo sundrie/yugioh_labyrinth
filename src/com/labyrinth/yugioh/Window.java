479da1090724ea810906efc53af8a05245b3f998
unknown <sundrie42@gmail.com>
2018-12-06 23:48:37 +0100

    J'utilise BoxLayout pour faire s'empiler mes boutons pour l'instant ils sont minuscules ce qui sera a fix

--- src/com/labyrinth/yugioh/Window.java
+++ src/com/labyrinth/yugioh/Window.java
@@ -6,11 +6,12 @@ public class Window extends JFrame {
     int width = 1280;
     int height = 720;
     String title = "Yu-Gi-Oh! Labyrinth";
+    private ContentPanel panel = new ContentPanel();
     // La méthode pour créer notre fenêtre
     public void createWindow(){
 
         // on dit à Window que notre classe ContentPanel est son contentPane
-        this.setContentPane(new ContentPanel());
+        this.setContentPane(panel);
 
         this.setTitle(title);
         this.setSize(width, height);
@@ -20,12 +21,22 @@ public class Window extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-        // JButton startButton = new JButton("Commencer");
-        this.add(new BtnStartingW("Commencer"));
+        JPanel b1 = new JPanel();
+        //On définit le layout en lui indiquant qu'il travaillera en ligne
+        b1.setLayout(new BoxLayout(b1, BoxLayout.LINE_AXIS));
+        b1.add(new BtnStartingW("Commencer"));
 
-        this.add(new BtnStartingW("Quitter"));
+        JPanel b2 = new JPanel();
+        //On définit le layout en lui indiquant qu'il travaillera en ligne
+        b2.setLayout(new BoxLayout(b2, BoxLayout.LINE_AXIS));
+        b2.add(new BtnStartingW("Quitter"));
 
-
+        //On positionne maintenant nos boutons en colonne
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+//        panel.add(new BtnStartingW("Commencer"));
+//        panel.add(new BtnStartingW("Quitter"));
+        panel.add(b1);
+        panel.add(b2);
 
         this.setVisible(true);
     }

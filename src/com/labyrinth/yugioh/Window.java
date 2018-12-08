eb9855fa2b99846fe6dc92fad1732e9b2b7f11d9
unknown <sundrie42@gmail.com>
2018-12-08 13:14:10 +0100

    Les boutons sont centrés sur la fenêtre

--- src/com/labyrinth/yugioh/Window.java
+++ src/com/labyrinth/yugioh/Window.java
@@ -1,6 +1,7 @@
 package com.labyrinth.yugioh;
 // On importe la librairie pour faire des fenêtres
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
     int width = 1280;
@@ -20,24 +21,25 @@ public class Window extends JFrame {
         // Termine le processus lorsqu'on clique sur la croix rouge
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-        JPanel b1 = new JPanel();
+        JPanel pbtn1 = new JPanel();
         //On définit le layout en lui indiquant qu'il travaillera en ligne
-        b1.setLayout(new BoxLayout(b1, BoxLayout.LINE_AXIS));
-        b1.add(new BtnStartingW("Commencer"));
+        pbtn1.setLayout(new BoxLayout(pbtn1, BoxLayout.LINE_AXIS));
+        pbtn1.add(new BtnStartingW("Commencer"));
+        System.out.println(pbtn1.getPreferredSize());
 
-        JPanel b2 = new JPanel();
-        //On définit le layout en lui indiquant qu'il travaillera en ligne
-        b2.setLayout(new BoxLayout(b2, BoxLayout.LINE_AXIS));
-        b2.add(new BtnStartingW("Quitter"));
+        JPanel pbtn2 = new JPanel();
+        pbtn2.setLayout(new BoxLayout(pbtn2, BoxLayout.LINE_AXIS));
+        pbtn2.add(new BtnStartingW("Quitter"));
 
+
+        JPanel btncontainer = new JPanel();
         //On positionne maintenant nos boutons en colonne
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-//        panel.add(new BtnStartingW("Commencer"));
-//        panel.add(new BtnStartingW("Quitter"));
-        panel.add(b1);
-        panel.add(b2);
-
+        // container.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        btncontainer.setLayout(new GridLayout(2, 1));
+        btncontainer.setPreferredSize(new Dimension(200,600));
+        btncontainer.add(pbtn1);
+        btncontainer.add(pbtn2);
+        panel.add(btncontainer);
         this.setVisible(true);
     }
 

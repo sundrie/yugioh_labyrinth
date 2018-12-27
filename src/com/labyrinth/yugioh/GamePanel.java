52213e493b2a0d8fde9b32982d0e2ac46828ecb7
unknown <sundrie42@gmail.com>
2018-12-28 00:42:43 +0100

    Ajout d'un autre jpanel qui servira à afficher les infos utiles au joueur (créatures description, créatures restantes,etc.)

--- src/com/labyrinth/yugioh/GamePanel.java
+++ src/com/labyrinth/yugioh/GamePanel.java
@@ -4,19 +4,35 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
+    JPanel mainPanel = this;
     // Ce JPanel sera notre écran de jeu principal qui contiendra le plateau de jeu
     JPanel gamePanelContainer = new JPanel();
+    // Celui ci servira a afficher toutes les infos utiles pour le joueur (description créatures, créatures restantes,etc.)
+    JPanel infoPanelContainer = new JPanel();
 
-    public void paintComponent(Graphics g) {
-        g.setColor(Color.gray);
-        g.fillRect(920,0,360,720);
-        g.setColor(Color.white);
-        g.drawString("Et là toutes les datas utiles au jeu",1000,720/2);
+//    public void paintComponent(Graphics g){
+//        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.LINE_AXIS));
+//        mainPanel.setSize(1280,720);
+//        gamePanelContainer.setBackground(Color.black);
+//        gamePanelContainer.setSize(920,720);
+//        infoPanelContainer.setBackground(Color.cyan);
+//        infoPanelContainer.setPreferredSize(new Dimension(360,720));
+//
+//        mainPanel.add(gamePanelContainer);
+//        mainPanel.add(infoPanelContainer);
+//    }
 
+    public void drawGamePanel(){
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.LINE_AXIS));
+        mainPanel.setSize(1280,720);
         gamePanelContainer.setBackground(Color.black);
         gamePanelContainer.setSize(920,720);
-        this.add(gamePanelContainer);
-        System.out.println(this);
+        infoPanelContainer.setBackground(Color.cyan);
+        infoPanelContainer.setPreferredSize(new Dimension(360,720));
+
+        mainPanel.add(gamePanelContainer);
+        mainPanel.add(infoPanelContainer);
     }
+
 
 }

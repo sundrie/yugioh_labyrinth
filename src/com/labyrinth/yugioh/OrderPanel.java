cef002ba3b0d279556be24791fcd7d7531ee8a21
Alexandre Blin <sundrie42@gmail.com>
2019-01-23 19:43:21 +0100

    Ajout d'un bouton à notre OrderPanel nommé wait et son text est Attendre

--- src/com/labyrinth/yugioh/OrderPanel.java
+++ src/com/labyrinth/yugioh/OrderPanel.java
@@ -14,12 +14,20 @@ public class OrderPanel extends JPanel implements MouseListener {
     }
 
     public OrderPanel () {
-
+        // On écoute la souris pour savoir quelle action à été cliquée
+        this.addMouseListener(this);
+        JButton wait = new JButton("Attendre");
+        add(wait);
+        wait.addMouseListener(this);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-
+        // Ceci trouvé ici https://stackoverflow.com/questions/21287661/java-getname-of-obj-that-activated-mouselistener
+        // sourcename renvoie le texte entré dans un JButton puisque source récuàère les événements des JButton uniquement
+        JButton source = (JButton) e.getSource();
+        String sourcename = source.getText();
+        System.out.println(sourcename);
     }
 
     @Override
